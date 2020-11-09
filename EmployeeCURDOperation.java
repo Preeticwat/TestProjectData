c3f73a17af70664f938ba90ce4a6d8d04664eb3c
Preeticwat <56227908+Preeticwat@users.noreply.github.com>
2020-11-09 15:26:39 +0530

    Add files via upload

--- EmployeeCURDOperation.java
+++ EmployeeCURDOperation.java
@@ -5,28 +5,32 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
+@RestController
 public class EmployeeCURDOperation {
 	
 	List<Employee> empList=new ArrayList<Employee>();
 	
+	@GetMapping("/getEmployeeData")
 	public void getEmployeeData(){
 		 System.out.println(empList);
 		
 	}
 
-	public void InsertEmployeeData(Employee emp){
+	@PostMapping("/InsertData")
+	public void InsertEmployeeData(@RequestBody  Employee emp){
 	    empList.add(emp);
 		System.out.println("data inserted successfully");
 		
 		
 	}
 
-	public void putEmployeeData(int empId ){
+	@PutMapping("/putdata")
+	public void putEmployeeData(@RequestParam int empId ,@RequestBody Employee emp){
 		
 		
 	}
-
-	public void deleteEmployeeData(int empId){
+    @DeleteMapping("/deleteData")
+	public void deleteEmployeeData(@RequestParam int empId){
 		int id= Optional.ofNullable(empId).get();
 		Iterator itr= empList.iterator();
 		while(itr.hasNext()){
